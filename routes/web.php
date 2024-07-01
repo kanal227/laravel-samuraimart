15e179b03794ea899d129eb9ba6651b418a1d669
kanal227 <gfa3894@gmail.com>
2024-07-02 00:22:46 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Contorollers\ProductController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
