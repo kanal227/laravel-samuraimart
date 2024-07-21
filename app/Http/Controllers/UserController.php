bedcd99ed36b288877b2bf925b82e53ac4e152ca
kanal227 <gfa3894@gmail.com>
2024-07-21 16:22:32 +0900

    お気に入り追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,4 +48,36 @@ class UserController extends Controller
         return to_route('mypage');
     }
 
+    public function update_password(Request $request)
+    {
+        $validateData = $request->validate([
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = Auth::user();
+
+        if ($request->input('password') == $request->input('password_confirmation')) {
+            $user->password = bcrypt($request->input('password'));
+            $user->update();
+        } else {
+            return to_route('mypage.edit_password');
+        }
+
+        return to_route('mypage');
+    }
+
+    public function edit_password()
+    {
+        return view('users.edit_password');
+    }
+
+    public function favorite()
+    {
+        $user = Auth::user();
+
+        $favorite_products = $user->favorite_products;
+
+        return view('users.favorite', compact('favorite_products'));
+    }
+
 }
